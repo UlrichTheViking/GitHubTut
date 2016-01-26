336d215e1272b18eb68d59375dab27ec83a7fa27
Ulrich <norstromu@gmail.com>
2016-01-26 12:44:12 -0500

    Initial Commit

---
+++ GitHubTut/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHubTut
+//
+//  Created by Nick Peterson on 1/26/16.
+//  Copyright © 2016 Dark Leviathan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
