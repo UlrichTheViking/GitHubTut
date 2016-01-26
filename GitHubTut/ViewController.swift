161658c1123f7cfc0ad60067ed327da16b81eebe
Ulrich <norstromu@gmail.com>
2016-01-26 13:00:39 -0500

    Second Change
    2nd1

--- GitHubTut/ViewController.swift
+++ GitHubTut/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
     // Do any additional setup after loading the view, typically from a nib.
     
     // Changes in master!!!
+    
+    // More changes in master :D
   }
 
   override func didReceiveMemoryWarning() {
